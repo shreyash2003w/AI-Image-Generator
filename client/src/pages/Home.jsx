cf8e991e2aee9b864edf5cf97985b05e3a80ae15
Shreyash Waghmare <118048447+shreyash2003w@users.noreply.github.com>
2023-07-07 23:21:31 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
         setLoading(true);
 
     try {
-        const response = await fetch('https://ai-image-generator-backend-three.vercel.app/api/v1/post',{
+        const response = await fetch('https://ai-image-generator-backend-phi.vercel.app/api/v1/post',{
             method:'GET',
             headers:{
                 'Content-Type':'application/json',
